867467de611d0ff09d98ca69c23696383e82d59c
Sebastian Garcia <fsmrrmemez@gmail.com>
2025-11-26 00:57:21 -0500

    Begun Implementing Player List

--- Assets/Scripts/LobbyNetClient.cs
+++ Assets/Scripts/LobbyNetClient.cs
@@ -4,12 +4,39 @@ using UnityEngine;
 using FishNet.Object;
 using FishNet.Connection;
 using Steamworks;
+using HeathenEngineering.SteamworksIntegration;
+using FishNet.Object.Synchronizing;
+using System;
 
 public class LobbyNetClient : NetworkBehaviour
 {
+    private readonly SyncVar<UserData> userData = new SyncVar<UserData>();
+
+    public UserData userDat
+    {
+        get { return userData.Value; }
+        set { userData.Value = value; }
+    }
+
+    private void Awake()
+    {
+
+        userData.OnChange += OnUserDataUpdated;
+
+    }
+
     void Start()
     {
-        
+
+    }
+
+    private void OnUserDataUpdated(UserData prev, UserData next, bool asServer)
+    {
+        next.LoadAvatar((args) =>
+        {
+            if (TitleManager.instance != null)
+                TitleManager.instance.onPlayerJoined.Invoke(next);
+        });
     }
 
     public override void OnSpawnServer(NetworkConnection connection)
@@ -17,6 +44,14 @@ public class LobbyNetClient : NetworkBehaviour
         base.OnSpawnServer(connection);
 
         BootstrapNetManager.instance.plrCount = SteamMatchmaking.GetNumLobbyMembers(new CSteamID(SteamManager.instance.lobbyID));
+    }
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+        userDat = UserData.Me;
+
+        Debug.Log("OnStartClient");
     }
 
     public override void OnDespawnServer(NetworkConnection connection)
@@ -30,7 +65,7 @@ public class LobbyNetClient : NetworkBehaviour
 
     void Update()
     {
-        
+
     }
 
 }
--- Assets/Scripts/TitleManager.cs
+++ Assets/Scripts/TitleManager.cs
@@ -1,13 +1,25 @@
+using FishNet.Connection;
+using HeathenEngineering.SteamworksIntegration;
+using Steamworks;
+using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class TitleManager : MonoBehaviour
 {
-    [SerializeField] private GameObject InitialButtons;
-    [SerializeField] private GameObject LobbyButtons;
-    [SerializeField] private TMP_Text LobbyTitle;
+    [SerializeField] private GameObject initialButtons;
+    [SerializeField] private GameObject lobbyButtons;
+    [SerializeField] private TMP_Text lobbyTitle;
+    [SerializeField] private GameObject playerList;
+    [SerializeField] private GameObject playerNodePrefab;
+    private Dictionary<string, GameObject> playerNodes;
+
+    [DoNotSerialize] public UnityEvent<UserData> onPlayerJoined;
+    [DoNotSerialize] public UnityEvent<UserData> onPlayerLeft;
 
     private EventSystem eventSystem;
 
@@ -16,13 +28,18 @@ public class TitleManager : MonoBehaviour
     private void Awake()
     {
         instance = this;
-        SteamManager.instance.LobbyTitle = LobbyTitle;
+        SteamManager.instance.LobbyTitle = lobbyTitle;
+
+        onPlayerJoined.AddListener(OnPlayerJoined);
+        onPlayerLeft.AddListener(OnPlayerLeft);
+
+        playerNodes = new Dictionary<string, GameObject>();
     }
 
     void Start()
     {
         eventSystem = GetComponent<EventSystem>();
-        LobbyButtons?.SetActive(false);
+        lobbyButtons?.SetActive(false);
 
         if (SteamManager.instance.GetLobbyID() != default)
             StartButton(null);
@@ -38,12 +55,12 @@ public class TitleManager : MonoBehaviour
         GameObject obj;
 
         if (buttonToSelect == null)
-            obj = LobbyButtons.GetComponentsInChildren<Button>()[0].gameObject;
+            obj = lobbyButtons.GetComponentsInChildren<Button>()[0].gameObject;
         else
             obj = buttonToSelect;
 
-        InitialButtons?.SetActive(false);
-        LobbyButtons?.SetActive(true);
+        initialButtons?.SetActive(false);
+        lobbyButtons?.SetActive(true);
         eventSystem?.SetSelectedGameObject(obj);
     }
 
@@ -52,12 +69,38 @@ public class TitleManager : MonoBehaviour
         GameObject obj;
 
         if (buttonToSelect == null)
-            obj = LobbyButtons.GetComponentsInChildren<Button>()[0].gameObject;
+            obj = lobbyButtons.GetComponentsInChildren<Button>()[0].gameObject;
         else
             obj = buttonToSelect;
 
-        LobbyButtons?.SetActive(false);
-        InitialButtons?.SetActive(true);
+        lobbyButtons?.SetActive(false);
+        initialButtons?.SetActive(true);
         eventSystem?.SetSelectedGameObject(obj);
+    }
+
+    private void OnPlayerJoined(UserData dat)
+    {
+        if (playerNodes.ContainsKey(dat.Nickname)) return;
+
+        GameObject newNode = Instantiate(playerNodePrefab);
+        newNode.transform.SetParent(playerList.transform, false);
+
+        TMP_Text name = newNode.GetComponentInChildren<TMP_Text>();
+        Image avatarImg = newNode.GetComponentInChildren<Image>();
+
+        if (name) name.text = dat.Nickname;
+        if (avatarImg) avatarImg.sprite = Sprite.Create(dat.Avatar, new Rect(0f, 0f, dat.Avatar.width, dat.Avatar.height), new Vector2(0.5f, 0.5f));
+
+        playerNodes[dat.Nickname] = newNode;
+    }
+
+    private void OnPlayerLeft(UserData dat)
+    {
+
+    }
+
+    private void OnDestroy()
+    {
+        
     }
 }
